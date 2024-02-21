99106edf1ea1232b83bacc4372d558d9540bfd0e
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-21 12:37:08 -0600

    art

---
+++ www/art/JS_Editor.js
@@ -0,0 +1,15 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Love is Art';
+var Room_ID = 'art';
+
+var AGE_RESTRICT = true;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://static.cinema-blue.icu/www/loader.js");
