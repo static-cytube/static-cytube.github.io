c57667292726b8a5936e4d38421386797ed959aa
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-21 12:49:03 -0600

    Update JS_Editor.js

--- www/sd/JS_Editor.js
+++ www/sd/JS_Editor.js
@@ -7,15 +7,7 @@
 var ChannelName_Caption = 'Sophia Diamond';
 var Room_ID = 'sd';
 
-
 var AGE_RESTRICT = false;
-
-var MOTD_RULES = false;
-var MOTD_ROOMS = false;
-
-var BOT_NICK = 'Chat_Bot';
-
-var ROOM_ANNOUNCEMENT = `This popular room just disappeared. To reclaim it send an email to <strong>admin@cinema-blue.icu</strong>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
