9711e005b71663b8f320e3c9f64649fe174e2444
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-21 14:15:39 -0600

    blk

--- www/blk/JS_Editor.js
+++ www/blk/JS_Editor.js
@@ -7,11 +7,7 @@
 var ChannelName_Caption = 'BLACKED';
 var Room_ID = 'blk';
 
-
-
 var MOTD_MSG = ``;
-
-var ROOM_ANNOUNCEMENT = ``;
 
 var ADVERTISEMENT = `<a href="https://join.blacked.com" target="_blank" rel="nofollow noopener">` + 
   `<img src="https://static.cinema-blue.icu/ads/blacked01.webp" title="BLACKED Jazlyn Ray" border="0"></a>` +
