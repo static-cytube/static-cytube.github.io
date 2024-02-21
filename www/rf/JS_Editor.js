2d98eab1709940d36d89b3dc54c445b93e611328
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-21 12:37:11 -0600

    Update JS_Editor.js

--- www/rf/JS_Editor.js
+++ www/rf/JS_Editor.js
@@ -7,9 +7,7 @@
 var ChannelName_Caption = 'Real Faces';
 var Room_ID = 'rf';
 
-
 var AGE_RESTRICT = true;
-
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
