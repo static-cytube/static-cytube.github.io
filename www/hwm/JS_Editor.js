5a43dc662a48cc84a4436bf3d22f0f1a0c8e8662
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-21 15:42:42 -0600

    Update JS_Editor.js

--- www/hwm/JS_Editor.js
+++ www/hwm/JS_Editor.js
@@ -4,11 +4,27 @@
 **@preserve
 */
 
-var ChannelName_Caption = 'Emily Monks';
-var Room_ID = 'em';
+var ChannelName_Caption = 'MILFs';
+var Room_ID = 'hwm';
 
+var ALLOW_GUESTS = true;
+var AGE_RESTRICT = true;
+var LOAD_BOT = true;
+// var BETA_USERS = ['lisaqtee'];
 
-var AGE_RESTRICT = false;
+var TRIVIA = false;
+
+// var HIGHLIGHT = [''];
+
+var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-free-male-masturbator" target="_blank">` + 
+  `<img src="https://jacncdn.github.io/ads/lovense01.webp" alt="Support this Room" border="0"></a><br />` +
+  `<strong>Sponsor</strong>`;
+
+// var ROOM_ANNOUNCEMENT = ``;
+
+// var MOD_ANNOUNCEMENT = `Admin is on vacation until March 13th.<br />If there are any problems please send an email to <strong>admin@jackandchat.net</strong><br />Thanks!`;
+
+var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
