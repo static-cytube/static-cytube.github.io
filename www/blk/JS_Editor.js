2f477ad14da0c85ec30c0bea017d1a5851523ca1
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-22 10:41:04 -0600

    upd

--- www/blk/JS_Editor.js
+++ www/blk/JS_Editor.js
@@ -6,12 +6,13 @@
 
 var ChannelName_Caption = 'BLACKED';
 var Room_ID = 'blk';
+var BOT_NICK = 'JackAndChatBot';
 
 var ADVERTISEMENT = `<a href="https://join.blacked.com" target="_blank" rel="nofollow noopener">` + 
   `<img src="https://static.cinema-blue.icu/ads/blacked01.webp" title="BLACKED Jazlyn Ray" border="0"></a>` +
   `<br />Advertisement</a>`;
 
-var MOTD_MSG = `RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2004)`;
+var MOTD_MSG = `<br /><span style="color:orange">RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2024)</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
--- www/hwm/JS_Editor.js
+++ www/hwm/JS_Editor.js
@@ -6,13 +6,11 @@
 
 var ChannelName_Caption = 'MILFs';
 var Room_ID = 'hwm';
+var BOT_NICK = 'JackAndChatBot';
 
-var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
 var LOAD_BOT = true;
 // var BETA_USERS = ['lisaqtee'];
-
-var TRIVIA = false;
 
 // var HIGHLIGHT = [''];
 
@@ -21,10 +19,9 @@ var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-fre
   `<strong>Sponsor</strong>`;
 
 // var ROOM_ANNOUNCEMENT = ``;
-
 // var MOD_ANNOUNCEMENT = `Admin is on vacation until March 13th.<br />If there are any problems please send an email to <strong>admin@jackandchat.net</strong><br />Thanks!`;
 
-var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
+var MOTD_MSG = `<br /><span style="color:orange">RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2024)</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
