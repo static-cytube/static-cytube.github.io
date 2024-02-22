80a12ad2bd338a1c9c29a28c23caf721fe9cc6de
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-02-22 09:12:01 -0600

    Update JS_Editor.js

--- www/blk/JS_Editor.js
+++ www/blk/JS_Editor.js
@@ -7,11 +7,11 @@
 var ChannelName_Caption = 'BLACKED';
 var Room_ID = 'blk';
 
-var MOTD_MSG = ``;
-
 var ADVERTISEMENT = `<a href="https://join.blacked.com" target="_blank" rel="nofollow noopener">` + 
   `<img src="https://static.cinema-blue.icu/ads/blacked01.webp" title="BLACKED Jazlyn Ray" border="0"></a>` +
   `<br />Advertisement</a>`;
+
+var MOTD_MSG = `RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2004)`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
