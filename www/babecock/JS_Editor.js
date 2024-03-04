bfbb10ff2426d2964a98453cf099a6586873ed19
jacncdn <cinema-blue@proton.me>
2024-03-04 09:45:05 -0600

    babecock

---
+++ www/babecock/JS_Editor.js
@@ -0,0 +1,17 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Emily Monks';
+var Room_ID = 'clone';
+
+
+var AGE_RESTRICT = false;
+
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://static.cinema-blue.icu/www/loader.js");
