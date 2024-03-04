56bf45627fff70f7ceec0c41a2a690baf9018f00
jacncdn <cinema-blue@proton.me>
2024-03-04 10:21:11 -0600

    Update JS_Editor.js

--- www/jac/JS_Editor.js
+++ www/jac/JS_Editor.js
@@ -7,27 +7,12 @@
 var ChannelName_Caption = 'Jack & Chat';
 var Room_ID = 'jac';
 
-
-
-var LOAD_BOT = false;
-var BOT_LOG = true;
 var BOT_NICK = "Cinema-Blue-Bot";
 
 var CHANNEL_DEBUG = true;
 var AGE_RESTRICT = false;
 
-var MOTD_RULES = true;
-var MOTD_ROOMS = false;
-
 // var BETA_USERS = ['Cinema-Blue-Bot','lisaqtee'];
-
-var TRIVIA = false;
-
-// var HIGHLIGHT = ['HIGHLIGHTS'];
-
-var ADVERTISEMENT = `<a href="http://www.swinglifestyle.com/?signup=FunWorksCouple" target="_blank">` + 
-  `<img src="https://static.cinema-blue.icu/ads/sls04.gif" alt="SwingLifeStyle" border="0"></a>` +
-  `<br /><a href="http://www.swinglifestyle.com/?signup=FunWorksCouple" target="_blank">Sponsor</a>`;
 
 var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-free-male-masturbator" target="_blank">` + 
   `<img src="https://static.cinema-blue.icu/ads/lovense01.webp" alt="Support this Room" border="0"></a><br />` +
@@ -42,9 +27,7 @@ var ROOM_ANNOUNCEMENT = `<br /><span style="color:Blue;font-weight:Bold;">Welcom
   `please contact <a style="font-weight:Bold" href="mailto:admin@cinema-blue.icu">admin@cinema-blue.icu</a></span><br />` +
   ``;
 
-// var MOD_ANNOUNCEMENT = `Lisa is on vacation until March 13th. If there are any problems send an email to <strong>admin@cinema-blue.icu</strong><br />Thanks!`;
-
-// var CLEAR_MSG = `Here is a list of other rooms https://s.lain.la/xZP2R `;
+// var MOD_ANNOUNCEMENT = `Admin is on vacation until March 13th. If there are any problems send an email to <strong>admin@cinema-blue.icu</strong><br />Thanks!`;
 
 // var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
 
