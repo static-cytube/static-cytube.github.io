9b684b230fb66674f317b9900d2fb49da5959d0d
jacncdn <cinema-blue@proton.me>
2024-03-13 12:23:36 -0500

    tg

---
+++ www/tg/JS_Editor.js
@@ -0,0 +1,15 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Thick Girls';
+var Room_ID = 'tg';
+
+var AGE_RESTRICT = false;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://static.cinema-blue.icu/www/loader.js");
