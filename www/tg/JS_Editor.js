0b5fe85da7e9859314ce948cd83ccde592a2b31f
jacncdn <cinema-blue@proton.me>
2024-03-19 08:55:16 -0500

    Update JS_Editor.js

--- www/tg/JS_Editor.js
+++ www/tg/JS_Editor.js
@@ -9,6 +9,7 @@ var ChannelName_Caption = 'Thick Girls';
 var Room_ID = 'tg';
 
 var AGE_RESTRICT = false;
+var UPDATE_PERMISSIONS = false;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
