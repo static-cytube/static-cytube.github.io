7450f48644d7ca246d6e667bf8a624553fb26dc8
jacncdn <cinema-blue@proton.me>
2024-04-02 15:01:54 -0500

    Create addVideos.js

---
+++ www/sbh/addVideos.js
@@ -0,0 +1,139 @@
+
+var newVideos = [ 
+{"url":"https://files.catbox.moe/13axcy.mp4"},
+{"url":"https://files.catbox.moe/4mnxvz.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/7/f/e/647fee777c023.mp4"},
+{"title":"Cock Worship - Blowbang 11","url":"https://media.hypnotube.com/videos/4/a/f/7/2/4af72e7f4c097fcc5bfbceec45bdbffc.m4v"},
+{"url":"https://media.hypnotube.com/videos/6/1/b/8/2/61b82a1c9562a.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/a/1/3/e/5a13e5712b974.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/9/2/c/5/592c50d050447-going-full-cocksucker.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/4/8/6/65486584ab0bc.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/d/d/e/63dde8812a764.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/d/c/c/63dcc880d1fa7.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/2/9/1/652910c547390.mp4"},
+{"url":"https://media.hypnotube.com/videos/1/2/6/5/9/1265929e76c9e7be.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/9/f/b/a/59fbac56d0180.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/b/2/6/0/5b26028f5c534.mp4"},
+{"url":"https://media.hypnotube.com/videos/1/2/6/5/9/1265929f3c12d6ab.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/e/8/9/4/5e8949efa2f39.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/f/e/7/1/5fe71855b0b72.mp4"},
+{"url":"https://media.hypnotube.com/videos/1/2/6/5/9/1265929e4cc6527e.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/e/9/3/d/5e93db20bf57e.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/3/c/8/633c82e2e455b.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/a/8/f/64a8fd9d3e5c5.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/1/4/4/641442e8141c9.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/5/8/9/635891915b07b.mp4"},
+{"url":"https://files.catbox.moe/9vnld7.mp4"},
+{"url":"https://files.catbox.moe/t4ohu3.mp4"},
+{"url":"https://files.catbox.moe/v0y698.mp4"},
+{"url":"https://files.catbox.moe/i3jomf.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/9/6/3/1/59631dff43a04-the-finisher.mp4"},
+{"url":"https://files.catbox.moe/pspgby.mp4"},
+{"url":"https://files.catbox.moe/1gzapz.mp4"},
+{"url":"https://files.catbox.moe/3g8eap.mp4"},
+{"url":"https://files.catbox.moe/lbh2wo.mp4"},
+{"url":"https://files.catbox.moe/86fcxb.mp4"},
+{"url":"https://files.catbox.moe/y9jn4v.mp4"},
+{"url":"https://media.hypnotube.com/videos/f/e/e/2/c/fee2c963dec8b48b3e25c1846482e4ac.mov"},
+{"url":"https://media.hypnotube.com/videos/6/4/b/2/6/64b26beb4332a.mp4"},
+{"url":"https://files.catbox.moe/pty8og.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/5/4/7/65547ddd622d9.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/a/c/e/64ace1fc30e58.mp4"},
+{"url":"https://files.catbox.moe/snpxwt.mp4"},
+{"url":"https://files.catbox.moe/vg0u8f.mp4"},
+{"url":"https://files.catbox.moe/7vpijn.mp4"},
+{"url":"https://files.catbox.moe/ffvp3y.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/9/c/2/639c2138af436.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/6/3/2/64632cfb2f6a2.mp4"},
+{"title":"Cock Worship - Blowbang 2","url":"https://media.hypnotube.com/videos/b/2/d/d/8/b2dd861f26ebb75d16ca4bccb28b3a5c.m4v"},
+{"url":"https://media.hypnotube.com/videos/6/1/0/2/b/6102b7a8a7879.mp4"},
+{"url":"https://files.catbox.moe/afgvzb.mp4"},
+{"url":"https://files.catbox.moe/f0lg1c.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/2/e/4/642e4ffb26305.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/9/f/8/3/59f8386e18b68.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/9/5/9/63959216adfb7.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/5/8/d/6458d74d676d9.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/2/8/4/9/62849c9d4e020.mp4"},
+{"url":"https://files.catbox.moe/zwsde1.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/8/0/0/64800cdb10900.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/3/e/d/e/63edea3c4f60f.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/c/1/3/f/5c13f5c459f43.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/c/0/d/f/5c0df394b9eff-cock-worship-juicy-dicks.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/0/3/8/640387fcb47bc.mp4"},
+{"title":"Cock Worship - A Dick Addicktion 6","url":"https://media.hypnotube.com/videos/4/1/5/4/3/41543bc5fc598f481f9efc62325a6f02.m4v"},
+{"url":"https://media.hypnotube.com/videos/6/4/b/4/b/64b4b7c158d11.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/1/3/7/65137fceb8c56.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/1/4/f/6514f8de0e523.mp4"},
+{"url":"https://files.catbox.moe/xipfax.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/5/c/5/1/65c511023915d.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/8/e/6/648e6ed1754bd.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/a/f/c/c/5afccdadf1b0a.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/b/8/6/0/5b8600bb21e16.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/c/9/a/6/5c9a659d57475.mp4"},
+{"url":"https://files.catbox.moe/d027ys.mp4"},
+{"url":"https://files.catbox.moe/q08yf3.mp4"},
+{"url":"https://files.catbox.moe/90qyy2.mp4"},
+{"url":"https://files.catbox.moe/kxlyax.mp4"},
+{"url":"https://files.catbox.moe/s15243.mp4"},
+{"url":"https://media.hypnotube.com/videos/5/f/2/e/1/5f2e1edfcb285.mp4"},
+{"url":"https://files.catbox.moe/umu8j6.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/9/6/1/6496189f04b7a.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/4/f/9/5/64f950ed5fd59.mp4"},
+{"url":"https://media.hypnotube.com/videos/6/2/7/a/c/627acf7cdb1f7.mp4"},
+
+  {}
+]; 
+ 
+let defDelay = 4000; 
+let delay = defDelay; 
+ 
+function findVideo(url) { 
+  let uid = -1; 
+  let justName = url.replace(/\.[^/.]+$/, ""); // Remove Extension 
+  justName = justName.replace(/[^0-9a-z]/gi, "."); // AlphaNumeric Only 
+  justName = justName.replace(/[.]+/gi,'.*'); 
+ 
+  let regex = new RegExp(".*" + justName + ".*", 'gi'); 
+ 
+  $('#queue > li.queue_entry').filter(function(){ 
+    if ($(this).html().match(regex)) { 
+      uid = $(this).data("uid"); 
+    } 
+  }); 
+ 
+  return uid; 
+} 
+ 
+function addNewVideos(loop) {
+  delay = defDelay; 
+  let video = newVideos[loop];
+  
+  if ((typeof video === "undefined") || (typeof video['url'] === "undefined")) {
+    window.console.log("Finished ################################################################################"); 
+    return; 
+  }
+  
+  let vUrl = video['url'];
+  let vType = "fi";
+  if (vUrl.indexOf("youtube.com") >= 0) { 
+    vType = "yt";
+    vUrl = vUrl.substring(vUrl.indexOf('=') + 1);
+  }
+  
+  let vTitle = video['title'];
+  if ((typeof vTitle === 'undefined') || (vTitle.length < 1)) { vTitle = vUrl; }
+  
+  let uid = findVideo(vUrl); 
+  if (uid >= 0) { 
+    window.console.log("Duplicate: " + vTitle); 
+    delay = 100; 
+  } else { 
+    window.console.log("Adding: " + vTitle); 
+    socket.emit("queue", { id: vUrl, title: vTitle, pos: "end", type: vType, "temp": false }); 
+  } 
+ 
+  setTimeout(()=>{ loop++; addNewVideos(loop); }, delay); 
+} 
+ 
+// socket.emit("clearPlaylist"); 
+addNewVideos(0); 
