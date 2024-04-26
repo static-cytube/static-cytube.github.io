47608148d346f4dc75634df61e6c893467786ba0
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-04-26 10:48:34 -0500

    ifm

---
+++ www/ifm/JS_Editor.js
@@ -0,0 +1,13 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'I Feel Myself';
+var Room_ID = 'ifm';
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://static.cinema-blue.icu/www/loader.js");
