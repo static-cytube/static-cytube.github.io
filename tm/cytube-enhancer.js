02c8fd0a2d7962692c8b98f0e51276d711456785
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-04-30 15:09:53 -0500

    Update cytube-enhancer.js

--- tm/cytube-enhancer.js
+++ tm/cytube-enhancer.js
@@ -36,7 +36,7 @@ safeWin.console.debug('##### CyTube Enhancer Loading v' + scriptVersion);
 
 let Base_URL = 'https://static.cinema-blue.icu/';
 
-jQuery('<link>').appendTo('head').attr({ type: 'text/css',  rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.css', });
+jQuery('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.css', });
 
 // ##################################################################################################################################
 
