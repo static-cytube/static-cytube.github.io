ecc93dad5bf9674582062cfbbe190b2facb10e58
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-11 10:14:33 -0500

    Update JS_Editor.js

--- www/hwm/JS_Editor.js
+++ www/hwm/JS_Editor.js
@@ -10,9 +10,6 @@ var BOT_NICK = 'JackAndChatBot';
 
 var AGE_RESTRICT = true;
 var LOAD_BOT = true;
-// var BETA_USERS = ['lisaqtee'];
-
-// var HIGHLIGHT = [''];
 
 var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-free-male-masturbator" target="_blank">` + 
   `<img src="https://jacncdn.github.io/ads/lovense01.webp" alt="Support this Room" border="0"></a><br />` +
@@ -20,7 +17,6 @@ var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-fre
 
 // var ROOM_ANNOUNCEMENT = ``;
 // var MOD_ANNOUNCEMENT = `Admin is on vacation until March 13th.<br />If there are any problems please send an email to <strong>admin@jackandchat.net</strong><br />Thanks!`;
-
 // var MOTD_MSG = `<br /><span style="color:orange">RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2024)</span>`;
 
 // ##################################################################################################################################
