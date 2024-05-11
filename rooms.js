57c2cd48938840aed3b538bbc4099cb980dd1086
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-11 10:46:27 -0500

    rooms init

---
+++ rooms.js
@@ -0,0 +1,42 @@
+/*!
+**|  CyTube Rooms
+**|  Version: 2024.05.11
+**@preserve
+*/
+"use strict";
+
+//  Channel Settings->Edit->JavaScript: jQuery.getScript("https://static.cinema-blue.icu/rooms.js");
+
+var Root_URL = "https://static.cinema-blue.icu/";
+var Base_URL = Root_URL + "www/";
+
+// ##################################################################################################################################
+
+const showRooms = function() {
+  $("#cytube_x").load(Root_URL + "inc/cytube_x.html");
+  $("#cytube_k").load(Root_URL + "inc/cytube_k.html");
+  $("#cytube_pg").load(Root_URL + "inc/cytube_pg.html");
+  $("#cytube_nn").load(Root_URL + "inc/cytube_nn.html");
+  $("#cytube_to").load(Root_URL + "inc/cytube_to.html");
+  $("#otherlists").load(Root_URL + "inc/otherlists.html");
+  $("#cytube_rooms").modal();
+};
+
+// ##################################################################################################################################
+
+if (typeof ROOMS_LOADED === "undefined") { // Only Load Once 
+  ROOMS_LOADED = true;
+
+  jQuery(document).ready(()=>{
+    jQuery("head").append('<link rel="stylesheet" type="text/css" id="roomscss" href="' + Root_URL + 'rooms.css" />');
+
+    jQuery(".navbar-brand").replaceWith('<span class="navbar-brand">' + CHANNELNAME + "</span>");
+    jQuery("ul.navbar-nav li:contains('Home')").remove();
+    jQuery("ul.navbar-nav li:contains('Discord')").remove();
+
+    jQuery.get(Base_URL + "cytube-rooms.html";, function(html_frag) { $('#pmbar').before(html_frag); });
+    jQuery("#nav-collapsible > ul").append('<li><a id="showrooms" href="javascript:void(0)" onclick="javascript:showRooms()">Rooms</a></li>');
+  });
+}
+
+// ##################################################################################################################################
