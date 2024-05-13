d11ac657129acb74d505c4329ef92676d59bca00
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-13 12:12:56 -0500

    rooms

--- rooms.js
+++ rooms.js
@@ -1,6 +1,6 @@
 /*!
 **|  CyTube Rooms
-**|  Version: 2024.05.11
+**|  Version: 2024.05.13
 **@preserve
 */
 "use strict";
@@ -19,7 +19,7 @@ const showRooms = function() {
   jQuery("#cytube_nn").load(Root_URL + "inc/cytube_nn.html");
   jQuery("#cytube_to").load(Root_URL + "inc/cytube_to.html");
   jQuery("#otherlists").load(Root_URL + "inc/otherlists.html");
-  jQuery("#cytube_rooms").modal();
+  jQuery("#cytube_rooms").modal('show');
 };
 
 // ##################################################################################################################################
@@ -28,6 +28,7 @@ if (typeof ROOMS_LOADED === "undefined") { // Only Load Once
   var ROOMS_LOADED = true;
 
   jQuery(document).ready(()=>{
+    // Add CSS Style Sheet for Rooms
     jQuery("head").append('<link rel="stylesheet" type="text/css" id="roomscss" href="' + Root_URL + 'rooms.css" />');
 
     jQuery(".navbar-brand").replaceWith('<span class="navbar-brand">' + CHANNELNAME + "</span>");
@@ -37,8 +38,9 @@ if (typeof ROOMS_LOADED === "undefined") { // Only Load Once
     // Add Rooms Button
     jQuery.get(Base_URL + "cytube-rooms.html", function(html_frag) { jQuery('#pmbar').before(html_frag); });
     jQuery("#nav-collapsible > ul").append('<li><a id="showrooms" href="javascript:void(0)" onclick="javascript:showRooms()">Rooms</a></li>');
+    jQuery("#cytube_rooms").on("click", function() { jQuery(this).modal('hide'); }); // Close after click
 
-    if (window.CLIENT.rank < Rank.Member) { // Add Register Button
+    if (window.CLIENT.rank < Rank.Member) { // If user NOT Registered then Add Register Button
       $('#nav-collapsible > ul').append('<li><a id="showregister" class="throb_text" target="_blank" href="/register">Register</a></li>');
     }
 
