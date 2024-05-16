5f25af274006dd10a3dbabf8b9b555fe510c9bd9
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-16 13:48:37 -0500

    rooms

--- rooms/rooms.js
+++ rooms/rooms.js
@@ -1,11 +1,10 @@
 /*!  CyTube Rooms
 **|  Version: 2024.05.16
-**|
+**|  Licensed under MIT
 **|  Channel Settings->Edit->JavaScript: jQuery.ajax({dataType:'script',cache:true,url:'https://static.cinema-blue.icu/rooms/rooms.min.js'});
 **@preserve
 */
 "use strict";
-
 
 var Root_URL = "https://static.cinema-blue.icu/rooms/";
 
