57b7be317dda74e5579ab529afefff51ce24bd64
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-21 13:43:01 -0500

    Update defaults.js

--- beta/defaults.js
+++ beta/defaults.js
@@ -197,7 +197,7 @@ CB.getFilters = function() {
     },
     success: function(data) {
       logTrace('defaults.getFilters', data);
-      debugData('Filters1', data);
+      logTrace('Filters1', JSON.stringify(data, null, 2));
       // customCSS = data;
       // setCustomCSS();
     },
