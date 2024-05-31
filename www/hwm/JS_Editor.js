03ccca3a85318bb48bfe8970940a50b705e42a2f
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-05-31 12:08:05 -0500

    Update JS_Editor.js

--- www/hwm/JS_Editor.js
+++ www/hwm/JS_Editor.js
@@ -1,6 +1,4 @@
-/*!
-**|  CyTube Customizations
-**|   
+/*! CyTube Customizations
 **@preserve
 */
 
@@ -10,14 +8,13 @@ var Room_ID = 'hwm';
 var BOT_NICK = 'JackAndChatBot';
 
 var ADVERTISEMENT = `<a href="https://www.lovense.com/solace-automatic-hands-free-male-masturbator" target="_blank">` + 
-  `<img src="https://jacncdn.github.io/ads/lovense01.webp" alt="Support this Room" border="0"></a><br />` +
+  `<img src="https://static.cinema-blue.icu/ads/lovense01.webp" alt="Support this Room" border="0"></a><br />` +
   `<strong>Sponsor</strong>`;
 
 // var ROOM_ANNOUNCEMENT = ``;
 // var MOD_ANNOUNCEMENT = `Admin is on vacation until March 13th.<br />If there are any problems please send an email to <strong>admin@jackandchat.net</strong><br />Thanks!`;
-// var MOTD_MSG = `<br /><span style="color:orange">RIP <a target="_blank" href="https://www.boobpedia.com/boobs/Kagney_Linn_Karter">Kagney Linn Karter</a> (1987-2024)</span>`;
+// var MOTD_MSG = ``;
 
 // ##################################################################################################################################
-if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
 
-jQuery.ajax({dataType:'script',cache:1,async:0,url:"https://static.cinema-blue.icu/www/loader.js",});
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
