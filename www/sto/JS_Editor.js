e7e729cf88d8c4c0b00bdddd8ae930660528c1f5
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-15 10:50:26 -0500

    Update JS_Editor.js

--- www/sto/JS_Editor.js
+++ www/sto/JS_Editor.js
@@ -5,9 +5,6 @@
 var ChannelName_Caption = 'Sheer Tryons';
 var Room_ID = 'sto';
 
-var AGE_RESTRICT = false;
-// var UPDATE_PERMISSIONS = false;
-
 // ##################################################################################################################################
 
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
