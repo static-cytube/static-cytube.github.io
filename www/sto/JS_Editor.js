386ab1a1bab8a79c0599b11146afe59932da9f8a
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-15 10:29:21 -0500

    sto

---
+++ www/sto/JS_Editor.js
@@ -0,0 +1,13 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Sheer Tryons';
+var Room_ID = 'sto';
+
+var AGE_RESTRICT = false;
+// var UPDATE_PERMISSIONS = false;
+
+// ##################################################################################################################################
+
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
