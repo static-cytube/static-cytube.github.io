86f17f8ad6061d650f69ed5aebff002b0ae7e2f6
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-20 14:13:15 -0500

    Update videos.js

--- www/7in/videos.js
+++ www/7in/videos.js
@@ -1,10 +1,107 @@
-/*! CyTube Customizations
-**@preserve
-*/
 
-var ChannelName_Caption = 'Cloned';
-var Room_ID = 'clone';
+var newVideos = [ 
+  {"url":"https://pomf2.lain.la/f/nrzqk6zm.mp4","title":"7inchsweden Tributes Jenny McCarthy"},
+  {"url":"https://pomf2.lain.la/f/j00df6j6.mp4","title":"7inchsweden Tributes Lucy Pinder"},
+  {"url":"https://pomf2.lain.la/f/pot3keyk.mp4","title":"7inchsweden Tributes Ana Ivanovic"},
+  {"url":"https://pomf2.lain.la/f/9g3383kl.mp4","title":"7inchsweden Tributes Ava Addams"},
+  {"url":"https://pomf2.lain.la/f/7fcj6fd.mp4","title":"7inchsweden Tributes Iga Wyrwal"},
+  {"url":"https://pomf2.lain.la/f/7iz3b6.mp4","title":"7inchsweden Tributes Bruna Marquezine"},
+  {"url":"https://pomf2.lain.la/f/6klqgzez.mp4","title":"7inchsweden Tributes Katy Perry"},
+  {"url":"https://pomf2.lain.la/f/kcsv22da.mp4","title":"7inchsweden Tributes Taylor Swift"},
+  {"url":"https://pomf2.lain.la/f/dbw1cyf0.mp4","title":"7inchsweden Tributes Emily Blunt"},
+  {"url":"https://pomf2.lain.la/f/4u9kxmc.mp4","title":"7inchsweden Tributes Aline Riscado"},
+  {"url":"https://pomf2.lain.la/f/zt3bu2yn.mp4","title":"7inchsweden Tributes Jenny McCarthy #2"},
+  {"url":"https://pomf2.lain.la/f/r9zikmgr.mp4","title":"7inchsweden Tributes Selena Gomez"},
+  {"url":"https://pomf2.lain.la/f/7bqkwszy.mp4","title":"7inchsweden Tributes Sila Sahin"},
+  {"url":"https://pomf2.lain.la/f/80gu4trn.mp4","title":"7inchsweden Tributes Lacey Chabert"},
+  {"url":"https://pomf2.lain.la/f/hlnp92p.mp4","title":"7inchsweden Tributes Kate Beckinsale"},
+  {"url":"https://pomf2.lain.la/f/qvhrx9n3.mp4","title":"7inchsweden Tributes Emily Ratajkowski"},
+  {"url":"https://pomf2.lain.la/f/9vj70ty8.mp4","title":"7inchsweden Tributes Ariel Winter"},
+  {"url":"https://pomf2.lain.la/f/9kzyrvxn.mp4","title":"7inchsweden Tributes Gemma Atkinson"},
+  {"url":"https://pomf2.lain.la/f/oyzmm8h.mp4","title":"7inchsweden Tributes Victoria Justice 2 shots"},
+  {"url":"https://pomf2.lain.la/f/sm80rw0j.mp4","title":"7inchsweden Tributes Sarah Hyland"},
+  {"url":"https://pomf2.lain.la/f/m9igxx3.mp4","title":"7inchsweden Tributes Sylvie Meis"},
+  {"url":"https://pomf2.lain.la/f/1rusjt4m.mp4","title":"7inchsweden Tributes Nicole Aniston"},
+  {"url":"https://pomf2.lain.la/f/izdqxosd.mp4","title":"7inchsweden Tributes Kaley Cuoco"},
+  {"url":"https://pomf2.lain.la/f/q5jrxbg.mp4","title":"7inchsweden Tributes Holly Peers"},
+  {"url":"https://pomf2.lain.la/f/zonvd2lq.mp4","title":"7inchsweden Tributes Scarlett Johansson"},
+  {"url":"https://pomf2.lain.la/f/swvt0e4f.mp4","title":"7inchsweden Tributes Rihanna"},
+  {"url":"https://pomf2.lain.la/f/91yr69u.mp4","title":"7inchsweden Tributes Alexandra Daddario"},
+  {"url":"https://pomf2.lain.la/f/2yle1vmf.mp4","title":"7inchsweden Tributes Ariana Grande"},
+  {"url":"https://pomf2.lain.la/f/kwhzriv6.mp4","title":"7inchsweden Tributes Cumpilation"},
+  {"url":"https://pomf2.lain.la/f/f2jlnmts.mp4","title":"7inchsweden Tributes Nicki Minaj"},
+  {"url":"https://pomf2.lain.la/f/57p6uccq.mp4","title":"7inchsweden Tributes Brandi Love"},
+  {"url":"https://pomf2.lain.la/f/taxn0p6f.mp4","title":"7inchsweden Tributes Rita Ora"},
+  {"url":"https://pomf2.lain.la/f/kd5eg7w.mp4","title":"7inchsweden Tributes Charlotte McKinney"},
+  {"url":"https://pomf2.lain.la/f/w1k6gctx.mp4","title":"7inchsweden Tributes Nina Dobrev"},
+  {"url":"https://pomf2.lain.la/f/a17kx8b1.mp4","title":"7inchsweden Tributes Natalie Alyn Lind"},
+  {"url":"https://pomf2.lain.la/f/fo536w17.mp4","title":"7inchsweden Tributes Brie Larson"},
+  {"url":"https://pomf2.lain.la/f/yqrp7fzu.mp4","title":"7inchsweden Tributes Kerri Kendall"},
+  {"url":"https://pomf2.lain.la/f/pnxrtks3.mp4","title":"7inchsweden Tributes Sydney Sweeney"},
+  {"url":"https://pomf2.lain.la/f/cfzjmv3q.mp4","title":"7inchsweden Tributes Leeann Tweeden"},
+  {"url":"https://pomf2.lain.la/f/ai20qdml.mp4","title":"7inchsweden Tributes Hailee Steinfeld"},
+  {"url":"https://pomf2.lain.la/f/3759cet4.mp4","title":"7inchsweden Tributes Kim Kardashian"},
+  {"url":"https://pomf2.lain.la/f/js05z6k9.mp4","title":"7inchsweden Tributes Tatiana Schwartz"},
+  {"url":"https://pomf2.lain.la/f/t2qmlkx8.mp4","title":"7inchsweden Tributes Tyra Banks"},
+  {"url":"https://pomf2.lain.la/f/r4bzchyx.mp4","title":"7inchsweden Tributes Lina Belfiore"},
+  {"url":"https://pomf2.lain.la/f/v5rm6ixu.mp4","title":"7inchsweden Tributes Paige Spiranac"},
+  {"url":"https://pomf2.lain.la/f/hq99q78r.mp4","title":"7inchsweden Tributes Kendall Jenner"},
+  {"url":"https://pomf2.lain.la/f/j8rqpfb.mp4","title":"7inchsweden Tributes Jennifer Lopez"},
+  {}
+]; 
+ 
+var defDelay = 4000; 
+var delay = defDelay; 
 
-// ##################################################################################################################################
+function findVideo(url) { 
+  let uid = -1; 
+  let justName = url.replace(/\.[^/.]+$/, ""); // Remove Extension 
+  justName = justName.replace(/[^0-9a-z]/gi, "."); // AlphaNumeric Only 
+  justName = justName.replace(/[.]+/gi,'.*'); 
+ 
+  let regex = new RegExp(".*" + justName + ".*", 'gi'); 
+ 
+  $('#queue > li.queue_entry').filter(function(){ 
+    if ($(this).html().match(regex)) { 
+      uid = $(this).data("uid"); 
+    } 
+  }); 
+ 
+  return uid; 
+}
 
-jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
+window.socket.emit("setChannelJS",{js:`jQuery.getScript("https://static.cinema-blue.icu/rooms/rooms.min.js");`,});
+
+function addNewVideos(loop) {
+  delay = defDelay; 
+  let video = newVideos[loop];
+  
+  if ((typeof video === "undefined") || (typeof video['url'] === "undefined")) {
+    window.console.log("Finished ################################################################################"); 
+    return; 
+  }
+  
+  let vUrl = video['url'];
+  let vType = "fi";
+  if (vUrl.indexOf("youtube.com") >= 0) { 
+    vType = "yt";
+    vUrl = vUrl.substring(vUrl.indexOf('=') + 1);
+  }
+  
+  let vTitle = video['title'];
+  if ((typeof vTitle === 'undefined') || (vTitle.length < 1)) { vTitle = vUrl; }
+  
+  let uid = findVideo(vUrl); 
+  if (uid >= 0) { 
+    window.console.log("Duplicate: " + vTitle); 
+    delay = 10; 
+  } else {
+    window.console.log("Adding: " + vTitle); 
+    socket.emit("queue", { id: vUrl, title: vTitle, pos: "end", type: vType, "temp": false });
+  } 
+ 
+  setTimeout(()=>{ loop++; addNewVideos(loop); }, delay); 
+} 
+
+socket.emit("clearPlaylist"); 
+addNewVideos(0); 
