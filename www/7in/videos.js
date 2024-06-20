6a8c43816b6a0ffd35fb61618fbdab27cb54d5ec
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-20 14:07:10 -0500

    Create videos.js

---
+++ www/7in/videos.js
@@ -0,0 +1,10 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Cloned';
+var Room_ID = 'clone';
+
+// ##################################################################################################################################
+
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
