c13057b17b22951fbaed67696adeac2599d966dd
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-20 14:08:08 -0500

    Update JS_Editor.js

--- www/gb/JS_Editor.js
+++ www/gb/JS_Editor.js
@@ -6,5 +6,4 @@ var ChannelName_Caption = 'Xtreme Fun';
 var Room_ID = 'gb';
 
 // ##################################################################################################################################
-
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
