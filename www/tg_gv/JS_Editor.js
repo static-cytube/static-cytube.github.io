730b9a69e79709f6ac70b688ddd19b1bd9cbf809
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-21 09:50:54 -0500

    tg_vg

---
+++ www/tg_gv/JS_Editor.js
@@ -0,0 +1,14 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Thick Girls';
+var Room_ID = 'tg_gv';
+
+var MOTD_RULES = false;
+var MOTD_ROOMS = true;
+var AGE_RESTRICT = false;
+// var UPDATE_PERMISSIONS = false;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
