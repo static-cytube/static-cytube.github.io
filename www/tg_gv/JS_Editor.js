95f479ff9b1431f6853c3d9aefc1706f70d4b902
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-06-21 10:12:21 -0500

    Update JS_Editor.js

--- www/tg_gv/JS_Editor.js
+++ www/tg_gv/JS_Editor.js
@@ -8,7 +8,7 @@ var Room_ID = 'tg_gv';
 var MOTD_RULES = false;
 var MOTD_ROOMS = true;
 var AGE_RESTRICT = false;
-// var UPDATE_PERMISSIONS = false;
+var UPDATE_PERMISSIONS = false;
 
 // ##################################################################################################################################
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
