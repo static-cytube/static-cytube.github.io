a72a634285abe35beb962462843ba49af1d2c947
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-07-17 10:44:04 -0500

    Create xxxclub-enhancer.js

---
+++ tm/xxxclub-enhancer.js
@@ -0,0 +1,127 @@
+// ==UserScript==
+// @name         XXXClub Enhancer
+// @namespace    https://cinema-blue.icu
+// @version      2024-07-17
+// @description  Add magnet to browse page
+// @author       You
+// @match        https://xxxclub.to/torrents/browse/*
+// @icon         https://xxxclub.to/assets/icons/favicon-16x16.png
+// @downloadURL  https://static.cinema-blue.icu/tm/xxxclub-enhancer.js
+// @updateURL    https://static.cinema-blue.icu/tm/xxxclub-enhancer.js
+// @license      MIT
+// @grant        unsafeWindow
+// @run-at       document-start
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+'use strict';
+
+// https://jshint.com/docs/options/
+// jshint curly:true, eqeqeq:true, esversion:10, freeze:true, futurehostile:true, latedef:true, maxerr:10, nocomma:true
+// jshint strict:global, trailingcomma:true, varstmt:true
+// jshint devel:true, jquery:true
+// jshint varstmt: false
+// jshint unused:false
+// jshint undef:true
+
+/* globals $, jQuery */
+
+const safeWin = window.unsafeWindow || window;
+
+const customCSS = `<style id="customCSS">
+@charset "UTF-8";
+
+.maincontainer {
+  max-width: 100%;
+  margin-left: 20%;
+}
+
+#modalOverlay {
+  position: fixed;
+  top: 4px;
+  left: 4px;
+  width: auto;
+  height: auto;
+  max-width: 20%;
+  z-index: 10;
+}
+
+.modalImg {
+  display: none;
+  width: auto;
+  height: auto;
+}
+</style>`;
+
+function popUnder(node) {
+    var newWindow = window.open("about:blank", node.target, "width=500,height=500");
+    newWindow.blur();
+    window.focus();
+    newWindow.location.href = node.href;
+    return false;
+}
+
+(function() {
+  'use strict';
+
+  const scriptName = GM_info.script.name;
+  const scriptVersion = GM_info.script.version;
+  safeWin.console.debug('##### ' + scriptName + ' Loading v' + scriptVersion);
+
+  window.attached = false;
+
+  safeWin.addEventListener("load", function() {
+    $('body').attr('class', 'night');
+    $('.recdiv').remove();
+    $('.recimg').remove();
+    $('head').append(customCSS);
+
+    var $modalOverlay = $('body').prepend('<div id="modalOverlay"></div>');
+
+    var $browse_list = $('.browsetableinside ul');
+    $browse_list.find('li').each(function() { $(this).find('span:first').remove(); }); // Remove "Category"
+    var $link_list = $browse_list.find('a[href^="/torrents/details/"]');
+
+    $link_list.each(function() {
+      let $link = $(this);
+      $link.before('<a title="Magnet Link" href="#"><label><i class="fa fa-magnet">&nbsp;</i></label></a>');
+
+      let $span = $link.parent();
+      let $target = $link.prev();
+      let $imgFloat = $link.next();
+
+      $link
+        .attr('target', '_blank')
+        .removeAttr('onpointerenter')
+        .removeAttr('onpointerleave');
+        // .on('click', function(node) { GM_openInTab(node.location.href, true); });
+
+      $span.on('pointerover', { id: $imgFloat.attr('id') }, function(e) {
+        $('#' + e.data.id).css('display', 'block');
+      });
+
+      $span.on('pointerout', { id: $imgFloat.attr('id') }, function(e) {
+        $('#' + e.data.id).css('display', 'none');
+      });
+
+      $imgFloat
+        .removeAttr('class').attr('class', 'modalImg')
+        .detach().appendTo('#modalOverlay');
+
+        $.ajax({
+        url: $(this).attr('href'),
+        cache: false,
+        error: function(data) { },
+        success: function(data) {
+          let magnetLink = $(data).find("a[href^='magnet:']").attr('href');
+          $target
+            .attr('href', magnetLink)
+            .css('color', 'red')
+            .on('click', function() { $(this).removeAttr('style'); });
+
+          let imgLink = $(data).find('.detailsposter').attr('src');
+          $imgFloat.attr('src', imgLink);
+        }
+      });
+    });
+  });
+})();
