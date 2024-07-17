00c164d5c8c2ef758a9a2cd241521da22ea5a247
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-07-17 15:21:09 -0500

    tf

---
+++ www/tf/JS_Editor.js
@@ -0,0 +1,10 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Tickle Fun';
+var Room_ID = 'tf';
+
+// ##################################################################################################################################
+
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
