843538d239ff2cb9f856c1b85445eee058282089
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-09-09 11:20:16 -0500

    Update JS_Editor.js

--- www/kk/JS_Editor.js
+++ www/kk/JS_Editor.js
@@ -1,10 +1,3 @@
-/*! CyTube Customizations
-**@preserve
-*/
-
 var ChannelName_Caption = 'Monroe';
 var Room_ID = 'kk';
-
-// ##################################################################################################################################
-
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
