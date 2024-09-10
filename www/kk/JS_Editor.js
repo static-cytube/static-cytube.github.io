6916c3ce54dddacaa469ea57b64c6828ecdea159
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-09-10 13:00:30 -0500

    Update JS_Editor.js

--- www/kk/JS_Editor.js
+++ www/kk/JS_Editor.js
@@ -1,5 +1,16 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
 var ChannelName_Caption = 'Monroe';
 var Room_ID = 'kk';
-var MOTD_RULES = false;
+
 var UPDATE_OPTIONS = false;
-jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
+
+var ROOM_ANNOUNCEMENT =
+  `If you are interested in being a <span style="color:Blue">Moderator</span> <br />` +
+  `contact <a style="font-weight:Bold" href="mailto:moderator@cinema-blue.icu">moderator@cinema-blue.icu</a></span><br />` +
+  ``;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/beta/loader.js");
