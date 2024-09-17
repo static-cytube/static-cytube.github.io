74b79f5269e778597803f2f871c06f1bd509c513
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-09-17 11:27:51 -0500

    Update loader.js

--- beta/loader.js
+++ beta/loader.js
@@ -5,7 +5,7 @@
 **|  Usage: Channel Settings->Edit->JavaScript: jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
 **@preserve
 */
-"use strict";
+'use strict';
 
 // https://jshint.com/docs/options/
 // jshint curly:true, eqeqeq:true, esversion:10, freeze:true, futurehostile:true, latedef:true, maxerr:10, nocomma:true
@@ -21,40 +21,43 @@ if (!window[window.CHANNEL.name]) { window[window.CHANNEL.name] = {}; }
 // Defaults
 var START = Date.now();
 var TODAY = new Date().toISOString().split('T')[0];
-if (typeof CBE === "undefined") { var CBE = {}; }
+if (typeof CBE === 'undefined') { var CBE = {}; }
 
-if (typeof ChannelName_Caption === "undefined") { var ChannelName_Caption = window.CHANNELNAME; }
-if (typeof Room_ID             === "undefined") { var Room_ID = "jac"; }
-if (typeof AGE_RESTRICT        === "undefined") { var AGE_RESTRICT = true; }
-if (typeof GUESTS_CHAT         === "undefined") { var GUESTS_CHAT = true; }
-if (typeof REPORT_EMAIL        === "undefined") { var REPORT_EMAIL = "admin@cinema-blue.icu"; }
+if (typeof ChannelName_Caption === 'undefined') { var ChannelName_Caption = window.CHANNELNAME; }
+if (typeof Room_ID             === 'undefined') { var Room_ID = 'jac'; }
+if (typeof AGE_RESTRICT        === 'undefined') { var AGE_RESTRICT = true; }
+if (typeof GUESTS_CHAT         === 'undefined') { var GUESTS_CHAT = true; }
+if (typeof REPORT_EMAIL        === 'undefined') { var REPORT_EMAIL = 'admin@cinema-blue.icu'; }
 
-if (typeof CHANNEL_DEBUG       === "undefined") { var CHANNEL_DEBUG = false; }
-if (typeof BETA_USER           === "undefined") { var BETA_USER = false; }
-if (typeof BETA_USERS          === "undefined") { var BETA_USERS = []; }
-if (typeof UPDATE_DEFAULTS     === "undefined") { var UPDATE_DEFAULTS = true; }
+if (typeof CHANNEL_DEBUG       === 'undefined') { var CHANNEL_DEBUG = false; }
+if (typeof BETA_USER           === 'undefined') { var BETA_USER = false; }
+if (typeof BETA_USERS          === 'undefined') { var BETA_USERS = []; }
+if (typeof UPDATE_DEFAULTS     === 'undefined') { var UPDATE_DEFAULTS = true; }
 
-if (typeof BOT_NICK            === "undefined") { var BOT_NICK = "Cinema-Blue-Bot"; }
-if (typeof ROOM_ANNOUNCEMENT   === "undefined") { var ROOM_ANNOUNCEMENT = ""; }
-if (typeof MOD_ANNOUNCEMENT    === "undefined") { var MOD_ANNOUNCEMENT = ""; }
-if (typeof ADVERTISEMENT       === "undefined") { var ADVERTISEMENT = ""; }
+if (typeof BOT_NICK            === 'undefined') { var BOT_NICK = 'Cinema-Blue-Bot'; }
+if (typeof ROOM_ANNOUNCEMENT   === 'undefined') { var ROOM_ANNOUNCEMENT = ''; }
+if (typeof MOD_ANNOUNCEMENT    === 'undefined') { var MOD_ANNOUNCEMENT = ''; }
+if (typeof ADVERTISEMENT       === 'undefined') { var ADVERTISEMENT = ''; }
 
-if (typeof MOTD_MSG            === "undefined") { var MOTD_MSG = ""; }
-if (typeof MOTD_RULES          === "undefined") { var MOTD_RULES = true; }
-if (typeof MOTD_ROOMS          === "undefined") { var MOTD_ROOMS = true; }
+if (typeof MOTD_MSG            === 'undefined') { var MOTD_MSG = ''; }
+if (typeof MOTD_RULES          === 'undefined') { var MOTD_RULES = true; }
+if (typeof MOTD_ROOMS          === 'undefined') { var MOTD_ROOMS = true; }
 
-if (typeof LOG_MSG === "undefined") { var LOG_MSG = (window.CLIENT.rank < window.Rank.Owner); }
+if (typeof LOG_MSG === 'undefined') { var LOG_MSG = (window.CLIENT.rank < window.Rank.Owner); }
 if (window.CLIENT.rank > window.Rank.Moderator) { LOG_MSG = false; } // NOT Owner+
 
 // jshint latedef:true
 
 // ##################################################################################################################################
 
-var Root_URL = "https://static.cinema-blue.icu/";
-var Base_URL = Root_URL + "www/";
-var Room_URL = Base_URL + Room_ID + "/";
+var Root_URL = 'https://static.cinema-blue.icu/';
+var Base_URL = Root_URL + 'www/';
+var Room_URL = Base_URL + Room_ID + '/';
 var CustomCSS_URL = Room_URL + 'custom.css'; // Used in common.js, defaults.js
-var minifyJS = document.currentScript.src.toLowerCase().includes(".min.");
+
+var loaderSrc = document.currentScript.src.toLowerCase();
+var minifyJS = loaderSrc.includes('.min.');
+// BETA_USER = loaderSrc.includes('/beta/');
 
 BETA_USERS = BETA_USERS.map(function(user) { return user.toLowerCase(); });
 if (BETA_USERS.indexOf(window.CLIENT.name.toLowerCase()) > -1) { BETA_USER = true; }
@@ -64,7 +67,7 @@ if (Room_ID.toLowerCase() === 'jac') { BETA_USER = true; }
 if (BETA_USER) {
   CHANNEL_DEBUG = true;
   minifyJS = false;
-  Base_URL = Base_URL.replace("/www/", "/beta/");
+  Base_URL = Base_URL.replace('/www/', '/beta/');
 }
 
 if (CHANNEL_DEBUG) { 
@@ -76,7 +79,7 @@ if (CHANNEL_DEBUG) {
 // ##################################################################################################################################
 
 $(document).ajaxError(function(event, jqxhr, settings, thrownError) {
-  window.console.error("AJAX Request Failed:", settings.url, thrownError);
+  window.console.error('AJAX Request Failed:', settings.url, thrownError);
 });
 
 // ##################################################################################################################################
@@ -86,25 +89,25 @@ CBE.urlParam = function(name) {
   if (!results || !results.length) { return null; }
   return results[1]
 };
-// var branch = !$.urlParam("beta") ? "www" : $.urlParam("beta");
+// var branch = !$.urlParam('beta') ? 'www' : $.urlParam('beta');
 
 // ##################################################################################################################################
 
 CBE.linkCSS = function(id, filename, minify = minifyJS) {
   try {
-    if (minify) { filename = filename.replace(".css", ".min.css"); }
+    if (minify) { filename = filename.replace('.css', '.min.css'); }
 
-    $("head").append('<link rel="stylesheet" type="text/css" id="' + id + '" href="' + filename + '?' + VERSION + '" />');
+    $('head').append('<link rel="stylesheet" type="text/css" id="' + id + '" href="' + filename + '?' + VERSION + '" />');
   } catch (e) {
-    window.console.error("loader.linkCSS error: " + filename + " - " + JSON.stringify(e));
+    window.console.error('loader.linkCSS error: ' + filename + ' - ' + JSON.stringify(e));
   }
 };
 
 // ##################################################################################################################################
 
 CBE.jsScripts = [
-  Base_URL + "common.js",
-  Base_URL + "showimg.js",
+  Base_URL + 'common.js',
+  Base_URL + 'showimg.js',
 ];
 
 // ----------------------------------------------------------------------------------------------------------------------------------
@@ -118,34 +121,34 @@ CBE.jsScripts = [
   Siteadmin: 255
 */
 
-if (typeof CUSTOM_LOADED === "undefined") { // Load Once
+if (typeof CUSTOM_LOADED === 'undefined') { // Load Once
   var CUSTOM_LOADED = true;
 
-  CBE.linkCSS("basecss", Base_URL + "base.css");
+  CBE.linkCSS('basecss', Base_URL + 'base.css');
 
   $('#chanexternalcss').detach().appendTo('head');  // Move down
 
-  CBE.linkCSS("customcss", CustomCSS_URL, false);
+  CBE.linkCSS('customcss', CustomCSS_URL, false);
 
-  $("#chancss").remove(); // Remove Adults Only Screen
+  $('#chancss').remove(); // Remove Adults Only Screen
 
   // ----------------------------------------------------------------------------------------------------------------------------------
   if (window.CLIENT.rank >= window.Rank.Admin) {
-    CBE.jsScripts.push(Base_URL + "betterpm.js");
-    if (UPDATE_DEFAULTS) { CBE.jsScripts.push(Base_URL + "defaults.js"); }
+    CBE.jsScripts.push(Base_URL + 'betterpm.js');
+    if (UPDATE_DEFAULTS) { CBE.jsScripts.push(Base_URL + 'defaults.js'); }
   }
 
   CBE.jsScripts.forEach(function(script) {
-    if (minifyJS) { script = script.replace(".js", ".min.js"); }
+    if (minifyJS) { script = script.replace('.js', '.min.js'); }
     jQuery.ajax({dataType: 'script', cache: true, async: true, timeout: 2000, url: script + '?' + VERSION, });
-    window.console.debug("loader.Script:", script);
+    window.console.debug('loader.Script:', script);
   });
 
   // ----------------------------------------------------------------------------------------------------------------------------------
   $(document).ready(function() {
-    $(".navbar-brand").replaceWith('<span class="navbar-brand">' + ChannelName_Caption + "</span>");
-    $("ul.navbar-nav li:contains('Home')").remove();
-    $("ul.navbar-nav li:contains('Discord')").remove();
+    $('.navbar-brand').replaceWith('<span class="navbar-brand">' + ChannelName_Caption + '</span>');
+    $('ul.navbar-nav li:contains("Home")').remove();
+    $('ul.navbar-nav li:contains("Discord")').remove();
   });
 }
 
