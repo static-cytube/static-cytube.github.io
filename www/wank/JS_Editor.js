e853085d745c94549021e1058b9a219bfcf3fd36
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-09-17 21:47:30 -0500

    wank

---
+++ www/wank/JS_Editor.js
@@ -0,0 +1,9 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Wank-O-Rama';
+var Room_ID = 'wank';
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
