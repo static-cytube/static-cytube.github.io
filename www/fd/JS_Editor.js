5d2ef4e19ecd2e1e324d67c2a5ced94cf0ce73cb
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-10-05 09:45:37 -0500

    Update JS_Editor.js

--- www/fd/JS_Editor.js
+++ www/fd/JS_Editor.js
@@ -7,12 +7,5 @@ var Room_ID = 'fd';
 
 var GUESTS_CHAT = false;
 
-var ROOM_ANNOUNCEMENT = `<br /><span style="color:blue"> ` +
-  `<strong>Ped0s</strong> got the last room banned. We're not replacing it again. ` +
-  `Keep your <i>under-age shit</i> out of here!!! <br /> <br /> ` +
-  `We will be checking the log files for violations and issuing <strong>lifetime BANS</strong> by name &amp; IP. ` + 
-  `&nbsp;</span>`;
-
 // ##################################################################################################################################
-
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
