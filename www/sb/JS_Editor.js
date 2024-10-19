75f45c5d3435570788659bffd502a14ed538caa8
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-10-19 11:07:51 -0500

    Update JS_Editor.js

--- www/sb/JS_Editor.js
+++ www/sb/JS_Editor.js
@@ -5,6 +5,7 @@
 var ChannelName_Caption = 'Solo Beauty';
 var Room_ID = 'sb';
 
-// ##################################################################################################################################
+var MOTD_MSG = `<span style="color:Orange"><strong>39 *NEW*</strong> Digital Desires videos added October 19th, 2024</span><br /><br />`;
 
+// ##################################################################################################################################
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
