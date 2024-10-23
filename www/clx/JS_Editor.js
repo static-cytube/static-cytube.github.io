991e6fe53773e715b859a2d7e3ae0d04eacdfffa
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2024-10-23 09:02:31 -0500

    Update JS_Editor.js

--- www/clx/JS_Editor.js
+++ www/clx/JS_Editor.js
@@ -7,6 +7,7 @@ var Room_ID = 'clx';
 
 var AGE_RESTRICT = true;
 
-// ##################################################################################################################################
+// var MOTD_MSG = `<span style="color:Orange"><strong>39 *NEW*</strong> videos added October 19th, 2024</span><br /><br />`;
 
+// ##################################################################################################################################
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
