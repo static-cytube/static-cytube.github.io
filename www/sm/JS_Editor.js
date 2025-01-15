b55f51303ea138a242c587b4327b8cbb960263e1
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-01-15 13:22:04 -0600

    Skinemax

---
+++ www/sm/JS_Editor.js
@@ -0,0 +1,11 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Skinemax';
+var Room_ID = 'sm';
+
+var AGE_RESTRICT = false;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
