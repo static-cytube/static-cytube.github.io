044302c2a6c148824ba6eeb1e092b5f07bf7547a
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-04-04 23:00:33 -0500

    ar

---
+++ www/ar/JS_Editor.js
@@ -0,0 +1,11 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Anna Ralphs';
+var Room_ID = 'am';
+
+var AGE_RESTRICT = true;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
