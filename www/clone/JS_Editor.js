76fcfe710c57e4632f219f10c9ce2c6b66639f8f
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-07-02 11:15:25 -0500

    Update JS_Editor.js

--- www/clone/JS_Editor.js
+++ www/clone/JS_Editor.js
@@ -6,5 +6,4 @@ var ChannelName_Caption = 'Cloned';
 var Room_ID = 'clone';
 
 // ##################################################################################################################################
-
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
