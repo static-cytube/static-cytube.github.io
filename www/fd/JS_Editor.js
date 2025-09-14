0beca9c2402640294491816c7e7373963ac6b5cd
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-09-14 14:27:42 -0500

    Update JS_Editor.js

--- www/fd/JS_Editor.js
+++ www/fd/JS_Editor.js
@@ -5,7 +5,5 @@
 var ChannelName_Caption = 'Kute Faces';
 var Room_ID = 'fd';
 
-var GUESTS_CHAT = false;
-
 // ##################################################################################################################################
 jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
