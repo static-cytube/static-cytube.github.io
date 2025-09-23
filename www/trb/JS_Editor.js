d8e27db8ce880e9bd1a649e6a01e54cbb1f64e2e
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-09-22 19:47:53 -0500

    trb

--- www/trb/JS_Editor.js
+++
@@ -1,4 +0,0 @@
-<div style="float:left">
-  <span style="color:White;font-size:175%;font-weight:500">Cum Tributes 18+</span><br />
-  <span style="color:yellow">ALL open chat and links must be legal (18+) or you will be <strong>BANNED!</strong></span><br />
-</div>
