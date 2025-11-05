d7b3acc108e21c4258f92c257e8d85bf32a9e9e6
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-11-04 18:39:25 -0600

    Update kosmi-enhancer.js

--- tm/kosmi-enhancer.js
+++ tm/kosmi-enhancer.js
@@ -3,7 +3,7 @@
 // @description  Change Kosmi for the better!
 // @author       Cinema-Blue
 // @copyright    2024+ Cinema-Blue
-// @version      2024-10-04
+// @version      2025-11-04
 // @license      MIT
 // @namespace    https://cinema-blue.icu
 // @icon         https://app.kosmi.io/favicon.png
@@ -31,47 +31,104 @@
 /* global jQuery */
 
 var safeWin = window.unsafeWindow || window;
+var debug = false;
 
 const scriptName = GM_info.script.name;
 const scriptVersion = GM_info.script.version;
 safeWin.console.debug('##### ' + scriptName + ' Loading v' + scriptVersion);
 
-// safeWin.console.debug(JSON.stringify(GM_info, null, 2));
+safeWin.console.debug(JSON.stringify(GM_info, null, 2));
 
-const btnClass = 'class="x6s0dn4 xur7f20 x1y0btm7 x9r1u3d xdh2fpr x1vjfegm x1ypdohk x78zum5 x1n2onr6 x1q0g3np ' +
-                 'x1l1c18b xif65rj xuxw1ft x9gmhs3 x117nqv4 xt7dq6l xl56j7k x193iq5w xu0aao5 x1a2a7pz xb3r6kr ' +
-                 'xwib8y2 x1igx7t4 x172e5tb x1y1aw1k x71s49j x87ps6o xeq5yr9 xoyzusl xa2dozc x1trvaba xwzlg83 ' +
-                 'xacu4zq x1hi3wi8 xm4o3sg xudkgsl x11jfisy x8qxh4v"';
+var $body = null;
+var $roomsDropDown = null;
+var $sidebar = null;
+var $sidebtns = null;
+var $hideVideo = null;
 
-safeWin.delayChanges = function() {
-  // safeWin.console.debug('##### ' + scriptName + ' delayChanges called');
+//  #################################################################################
 
-  try {
-    let sidebar = jQuery(".sidebar-tYw").first();
-    if (typeof sidebar !== 'undefined') {
-      sidebar.css("width", "600px");
-    }
+var mutationConfig = {
+  childList: true,
+  subtree: true,
+};
 
-    let msgBox = jQuery('[class^="messagesDropdownMenu"]').first(); // ".messagesDropdownMenu-k+8"
-    if (typeof msgBox !== 'undefined') {
-      msgBox.css("width", "600px");
-      msgBox.css("height", "540px");
-      msgBox.css("maxHeight", "540px");
-    }
+// --------------------------------------------------------------------------------
+function handleVideo(videoElement) {
+  // safeWin.console.debug(`######  Observe Tag: ${videoElement.tagName}`);
 
-    if (jQuery('#cleancams').length === 0) {
-      jQuery(`<button type="button" id="cleancams" ${btnClass}>Clean Cams</button>`)
-        .appendTo(".leftButtons-XpD").first()
-        .on("click", function() {
-          let webCams = jQuery('[class^="webcamOverlayItem"]');
-          for (let i = 0; i < webCams.length; i++) {
-            webCams[i].style.top = "6%";
-            webCams[i].style.left = "2%";
-            webCams[i].style.width = "80px";
-            webCams[i].style.height = "80px";
-          }
+  if (videoElement.tagName === 'VIDEO') {
+    $(videoElement).on('play', function() {
+
+      videoElement.muted = true;
+      videoElement.volume = 0.0;
+
+      if (videoElement.src !== "") {
+        videoElement.id = "mainvideo";
+        videoElement.volume = 0.05;
+        videoElement.style.display = 'none';
+     } else {
+        var cam = $(videoElement).closest('DIV'); // parent DIV
+        cam.addClass('camVideo');
+        cam.removeAttr('style');
+        cam.css({
+          'top': "1%",
+          'left': "1%",
+          'width': '80px',
+          'height': '80px',
         });
+      }
+
+      if (this.src) {
+        safeWin.console.log('Video:', (this.src || 'Source Unknown'));
+      }
+    });
+
+    // safeWin.console.debug('Observer attached:', videoElement);
+    // safeWin.console.dir(videoElement);
+
+    videoElement.muted = true;
+  }
+}
+
+// --------------------------------------------------------------------------------
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes) {
+      mutation.addedNodes.forEach(function(addedNode) {
+        handleVideo(addedNode);
+
+        if (addedNode.querySelectorAll) {
+          addedNode.querySelectorAll('video').forEach(handleVideo);
+        }
+      });
     }
+  });
+});
+
+//  #################################################################################
+safeWin.delayChanges = function() {
+  try {
+    if ($roomsDropDown === null) {
+      $roomsDropDown = jQuery('i.caret.down').first().parents('div').eq(1).attr('id', 'roomsDropDown');
+    }
+
+    // Add 'Toggle Video'
+    if ($hideVideo === null) {
+      $hideVideo = jQuery('#roomsDropDown').after('<button type="button" id="hideVideo">Video</button>');
+      jQuery('#hideVideo').on('click', function() {
+        jQuery('#mainvideo').toggle();
+      });
+    }
+
+    if ($sidebar === null) {
+      $sidebar = jQuery('i.ellipsis.horizontal').first().parents('div').eq(3).attr('id', 'sidebar');
+    }
+    $sidebar.css("width", "500px");
+
+    if ($sidebtns === null) {
+      $sidebtns = jQuery('i.ellipsis.horizontal').first().parents('div').eq(2).attr('id', 'sidebtns');
+    }
+    $sidebtns.hide();
 
   } catch (error) {
     safeWin.console.error('##### ' + scriptName + ' delayChanges: ' + error + ' at line: ' + error.lineNumber);
@@ -80,12 +137,21 @@ safeWin.delayChanges = function() {
   }
 };
 
+//  #################################################################################
 safeWin.addEventListener("load", function(){
   try {
-    setInterval(function() { safeWin.delayChanges(); }, 6000);
+    $body = document.body;
+    observer.observe($body, mutationConfig); // Start observing
+
+    if (debug) { jQuery('body').append('<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>'); }
+
+    setInterval(function() { safeWin.delayChanges(); }, 2000);
   } catch (error) {
     safeWin.console.error('##### ' + scriptName + ' DocReady: ' + error + ' at line: ' + error.lineNumber);
     safeWin.console.dir(error);
     debugger;
   }
 });
+
+//  #################################################################################
+//  #################################################################################
