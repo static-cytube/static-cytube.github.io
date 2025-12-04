91c109c94a44da7b2f7e4a1994ee372989dfc7e4
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2025-12-04 12:45:54 -0600

    Update JS_Editor.js

--- www/blk/JS_Editor.js
+++ www/blk/JS_Editor.js
@@ -4,7 +4,7 @@
 
 var ChannelName_Caption = 'BLACKED';
 var Room_ID = 'blk';
-var BOT_NICK = 'JackAndChatBot';
+var GUESTS_CHAT = false;
 
 var ADVERTISEMENT = `<a href="https://join.blacked.com" target="_blank" rel="nofollow noopener">` + 
   `<img src="https://static.cinema-blue.icu/ads/blacked01.webp" title="BLACKED Jazlyn Ray" border="0"></a>` +
