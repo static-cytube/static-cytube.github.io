f3a137bcf0591348de1c34b1baea7a5fb6387123
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2026-01-10 15:23:58 -0600

    cinema-blue

---
+++ www/cb/JS_Editor.js
@@ -0,0 +1,11 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Cinema Blue';
+var Room_ID = 'cl';
+
+var AGE_RESTRICT = true;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
