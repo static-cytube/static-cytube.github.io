24c1878b6627bcf1317b80d2f77bab7b65cd9557
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2026-01-11 12:41:23 -0600

    LesbianBeauties

---
+++ www/lb/JS_Editor.js
@@ -0,0 +1,9 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Lesbian Beauties';
+var Room_ID = 'lb';
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
