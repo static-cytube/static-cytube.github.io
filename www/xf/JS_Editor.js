002d92ec13fe29e59a5e0c626bb479c36b75ab3e
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2026-02-20 08:31:59 -0600

    xf

---
+++ www/xf/JS_Editor.js
@@ -0,0 +1,9 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Xtreme Fun';
+var Room_ID = 'xf';
+
+// ##################################################################################################################################
+jQuery.getScript("https://static.cinema-blue.icu/www/loader.min.js");
