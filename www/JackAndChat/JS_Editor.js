e9e112f36ea7f597bb2f9547fb40ac033b498899
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2026-03-03 09:52:43 -0600

    jac

---
+++ www/JackAndChat/JS_Editor.js
@@ -0,0 +1,29 @@
+/*! CyTube Customizations
+**@preserve
+*/
+
+var ChannelName_Caption = 'Jack & Chat';
+// var Room_ID = 'jac';
+
+var CHANNEL_DEBUG = true;
+var UPDATE_DEFAULTS = false;
+var UPDATE_JS = false;
+
+var AGE_RESTRICT = false;
+
+// var BETA_USERS = ['Cinema-Blue-Bot','lisaqtee'];
+
+var ADVERTISEMENT = `<a href="http://www.swinglifestyle.com/?signup=FunWorksCouple" target="_blank">` + 
+  `<img src="https://static-cytube.github.io/ads/sls04.gif" alt="SwingLifeStyle" border="0"></a>` +
+  `<br /><a href="http://www.swinglifestyle.com/?signup=FunWorksCouple" target="_blank">Sponsor</a>`;
+
+var ROOM_ANNOUNCEMENT =
+  `If you are interested in being a <span style="color:Blue">Moderator</span>, have a Comment or <em>Constructive</em> Criticism,<br />` +
+  `please contact <a style="font-weight:Bold" href="mailto:admin@cinema-blue.icu">admin@cinema-blue.icu</a></span><br />` +
+  ``;
+
+// var MOD_ANNOUNCEMENT = `Lisa is on vacation until March 13th. If there are any problems send an email to <strong>admin@cinema-blue.icu</strong><br />Thanks!`;
+var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
+
+// ##################################################################################################################################
+jQuery.getScript("https://static-cytube.github.io/beta/loader.js");
