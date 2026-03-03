7fee6d96e04317c939a2403551c855b50d7ce926
static-cinema-blue <160078726+static-cytube@users.noreply.github.com>
2026-03-03 16:59:10 -0600

    sync

--- www/bb/JS_Editor.js
+++
@@ -1,9 +0,0 @@
-/*! CyTube Customizations
-**@preserve
-*/
-
-var ChannelName_Caption = 'Best Buddies';
-var Room_ID = 'bb';
-
-// ##################################################################################################################################
-jQuery.getScript("https://static-cytube.github.io/www/loader.min.js");
